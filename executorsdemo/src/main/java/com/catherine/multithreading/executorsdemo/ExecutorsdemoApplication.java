60d8f9148cf338d08494fa4303321afca71a5bfe
catherinechristyc@gmail.com <15June1995@>
2022-06-18 13:39:02 +0530

    adding

---
+++ executorsdemo/src/main/java/com/catherine/multithreading/executorsdemo/ExecutorsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.catherine.multithreading.executorsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutorsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutorsdemoApplication.class, args);
+	}
+
+}
---
+++ executorsdemo/src/test/java/com/catherine/multithreading/executorsdemo/ExecutorsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catherine.multithreading.executorsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutorsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
